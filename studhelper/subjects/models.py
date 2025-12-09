ab65219b02df1dc76a95633639e439f199e0f4ec
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-09 22:49:32 +0300

    created models Subject, Topic, Flashcard

--- studhelper/subjects/models.py
+++ studhelper/subjects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
--- studhelper/topics/models.py
+++ studhelper/topics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    subject = models.ForeignKey('subjects.Subject', on_delete=models.CASCADE, null=True) # Связь с моделью Subject(null=True для тестирования, потом обязательно поменять!)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Flashcard(models.Model):
+    quation = models.TextField()
+    answer = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.quation
