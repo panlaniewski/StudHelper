fae6680cd96a1ad9d8d45385450834dad40b8efb
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-09 21:22:47 +0300

    created model User

--- studhelper/core/models.py
+++ studhelper/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # AbstractUser уже содержит: username, first_name, last_name, email, password, is_staff, is_active, is_superuser, last_login, date_joined
+    email = models.EmailField('email address', unique=True)
+    is_email_verified = models.BooleanField(default=False)
+
+
+    
+
