f17900d34b2a00a5f6f079cdbea61c49806a1ef2
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-09 23:04:29 +0300

    change User model

--- studhelper/core/models.py
+++ studhelper/core/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    # AbstractUser уже содержит: username, first_name, last_name, email, password, is_staff, is_active, is_superuser, last_login, date_joined
+class User():
+    username =models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     email = models.EmailField('email address', unique=True)
-    is_email_verified = models.BooleanField(default=False)
-
+    password = models.CharField(max_length=128)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     
 
