c5241a707cab8cf7eea53084ef16d15b15c9c3a4
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-11 02:21:27 +0300

    add basic authorization(create app users), doesn't work yet

--- studhelper/studhelper/settings.py
+++ studhelper/studhelper/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
     'core',
     'subjects',
     'topics',
+    'users',
 ]
 
 MIDDLEWARE = [
--- studhelper/studhelper/urls.py
+++ studhelper/studhelper/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
     path('subjects/', include("subjects.urls")),
+    path('users/', include("users.urls", namespace="users")),
 ]
 
 handler404 = page_not_found
