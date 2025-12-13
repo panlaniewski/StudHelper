ae76f664caefad728a48f7ac8224f550360e2afc
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-13 21:00:02 +0300

    implemented registration (exit is not working yet)

--- studhelper/users/views.py
+++ studhelper/users/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import LoginView
-from django.urls import reverse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name = 'login.html'
     extra_context = {'title': 'Авторизация'}
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'], password=cd['password'])
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
-#     else:
-#         form = LoginUserForm()
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('users:login')
     
-#     return render(request, 'users/login.html', {'form': form})
-
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('users:login'))
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegisterUserForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.set_password(form.cleaned_data['password'])
+#             user.save()
+#             return render(request, 'register_done.html')
+#     else:
+#         form = RegisterUserForm()
+    
+#     return render(request, 'register.html', {'form': form})
