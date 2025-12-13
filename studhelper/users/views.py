14586796696972b2aec837c24e8d84befaf980ce
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-13 17:42:28 +0300

    started using LoginView, LogoutView, AuthenticationForm for authorization

--- studhelper/users/views.py
+++ studhelper/users/views.py
@@ -2,22 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import LoginUserForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
 from django.urls import reverse
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        form = LoginUserForm()
-    
-    return render(request, 'users/login.html', {'form': form})
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
 
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('users:login'))
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'], password=cd['password'])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse('home'))
+#     else:
+#         form = LoginUserForm()
+    
+#     return render(request, 'users/login.html', {'form': form})
+
+# def logout_user(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse('users:login'))
