fa59dd358b70c2975e5fe46e0ffb8b635dc72c2d
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-14 20:53:53 +0300

    connect Subject model with User model and make migrations

--- studhelper/topics/models.py
+++ studhelper/topics/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # ----------------------------------------------------------------------------------------------------------------------    
 class Topic(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    subject = models.ForeignKey('subjects.Subject', on_delete=models.CASCADE, null=True) 
+    subject = models.ForeignKey('subjects.Subject', on_delete=models.CASCADE, null=True, blank=True) 
 
     def __str__(self):
         return self.name
@@ -10,7 +10,7 @@ class Topic(models.Model):
 class Flashcard(models.Model):
     quation = models.TextField()
     answer = models.TextField()
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.quation
