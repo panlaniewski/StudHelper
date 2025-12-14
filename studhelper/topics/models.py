bc17899e138dd165431ddc9d9ccb282a9e1b0a35
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-12-14 23:21:00 +0300

    Fix conflict2

--- studhelper/topics/models.py
+++ studhelper/topics/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 # ----------------------------------------------------------------------------------------------------------------------    
 class Topic(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    # workbook = models.TextField(blank=True)
-    subject = models.ForeignKey('subjects.Subject', on_delete=models.CASCADE, null=True) 
+    name = models.CharField(max_length=200)
+    subject = models.ForeignKey('subjects.Subject', on_delete=models.CASCADE, null=True, blank=True) 
 
     def __str__(self):
         return self.name
