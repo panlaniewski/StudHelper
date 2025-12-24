dadda292917b774bd7e12f526fcc173cb0ea542c
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-12-24 12:11:37 +0300

    added the ability to edit a synopsis

--- studhelper/users/forms.py
+++ studhelper/users/forms.py
@@ -22,23 +22,24 @@ class RegisterUserForm(UserCreationForm):
         model = get_user_model()
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
-    # labels = {
-    #     'email': "E-mail",
-    #     'first_name': "Имя",
-    #     'last_name': "Фамилия"
-    # }
+        # labels = {
+        #     'email': "E-mail",
+        #     'first_name': "Имя",
+        #     'last_name': "Фамилия"
+        # }
 
-    # widgets = {
-    #     'email': forms.EmailInput(attrs={'class': 'form-control'}),
-    #     'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-    #     'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-    # }
+        # widgets = {
+        #     'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        #     'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        # }
 
     def clean_email(self):
         email = self.cleaned_data['email']
-        if get_user_model().objects.filter(email=email).exists():
-            raise forms.ValidationError("Такой e-mail уже существует!")
-        return email
+        if email not in [None, '']:     # валидация только если поле не пустое
+            if get_user_model().objects.filter(email=email).exists():
+                raise forms.ValidationError("Такой e-mail уже существует!")
+            return email
 
 class ProfileUserForm(forms.ModelForm):
     username = forms.CharField(disabled=True, label="Логин", widget=forms.TextInput(attrs={'class': 'form-control'}))
